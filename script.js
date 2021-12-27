7c8707f4f1fe758b7e31a94c6e86660c0e92750d
McBallins <warier@live.com>
2021-12-27 11:08:57 -0600

    fixed formulas and added operate formula

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
-function add (a, b) {
+function add(a, b) {
     return a+b;
 };
 
-function subtract (a, b) {
-    return a+b;
+function subtract(a, b) {
+    return a-b;
 };
 
-function multiply (a, b) {
-    return a+b;
+function multiply(a, b) {
+    return a*b;
 };
 
-function divide (a, b) {
-    return a+b;
+function divide(a, b) {
+    return a/b;
+};
+
+function operate(a, b, o) {
+    if(o === '+') {
+        return add(a, b);
+    } else if(o === '-') {
+        return subtract(a, b);
+    } else if(o === '*') {
+        return multiply(a, b);
+    } else if(o === '/') {
+        return divide(a, b);
+    } else {
+        return undefined;
+    };
 };
