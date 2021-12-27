05a0e9de57470d2f833cd8e1aeba5960cf6fc064
McBallins <warier@live.com>
2021-12-27 13:38:03 -0600

    mostly done setting event listeners

--- script.js
+++ script.js
@@ -1,5 +1,42 @@
-
 const display = document.getElementById('display');
+
+let integerA;
+let integerB;
+
+const integers = document.querySelectorAll('.integer');
+
+integers.forEach(integer => {
+    integer.addEventListener('click', event => {
+        if(integerA === undefined) {
+            integerA = integer.innerText;
+        } else {
+            integerB = integer.innerText;
+        }
+        console.log(`${integerA}, ${integerB}`);
+    });
+});
+
+let operatorVal;
+const operators = document.querySelectorAll('.operator');
+operators.forEach(operator => {
+    operator.addEventListener('click', event => {
+        operatorVal = operator.innerText;
+        console.log(operatorVal);
+    });
+});
+
+const equals = document.getElementById('equals')
+equals.addEventListener('click', function eval() {
+    operate(integerA, integerB, operator);
+});
+
+
+const clear = document.getElementById('clear');
+clear.addEventListener('click', function clear() {
+    display.innerText = '0';
+    integerA = undefined;
+    integerB = undefined;
+});
 
 function add(a, b) {
     return a+b;
@@ -34,25 +71,3 @@ function operate(a, o, b) {
     return value;
 };
 
-//Event listeners for buttons, i will need a switch here that changes which variable is being set
-let integerA;
-let integerB;
-integers = document.querySelectorAll('.integer');
-
-function setA() {
-    integers.forEach(integer => {
-        integer.addEventListener('click', event => {
-            integerA = integer.innerText;
-            console.log(integerA);
-    });
-});
-};
-
-function setB() {
-    integers.forEach(integer => {
-        integer.addEventListener('click', event => {
-            integerA = integer.innerText;
-            console.log(integerB);
-    });
-});
-};
